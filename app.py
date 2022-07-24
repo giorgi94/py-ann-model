64fcf62e11f66f1e6c2b48f8715823c178631cb0
giorgi94 <giorgik1994@gmail.com>
2022-07-24 16:19:39 +0400

    it is working

--- app.py
+++ app.py
@@ -21,16 +21,57 @@ class LinearLayer:
         return self.weight.dot(x) + self.bias
 
 
-x = np.array([[0.3], [0.1], [0.4]], dtype=np.float32)
-y = np.array([[0.8], [0.9]], dtype=np.float32)
+x = np.array([[0.3, 0.1, 0.4]], dtype=np.float32).T
+y = np.array([[0.8341, 0.9421]], dtype=np.float32).T
 
-weight = np.random.rand(2, 3)
-bias = np.random.rand(2, 1)
-
-layer = LinearLayer(weight, bias)
-
-z = layer.calc(x)
-a = sigmoid(x)
+x.flags.writeable = False
+y.flags.writeable = False
 
 
-print(z)
+def train():
+    weight = np.random.rand(2, 3)
+    bias = np.random.rand(2, 1)
+
+    learning_rate = 0.05
+
+    epoch_size = 5000
+
+    for epoch in range(epoch_size):
+
+        z = weight.dot(x) + bias
+
+        y_predicted = sigmoid(z)
+
+        delta = y_predicted - y
+
+        s = np.diag(sigmoid(z, True).flatten())
+
+        error = (delta**2).sum() / 2
+
+        # if (epoch + 1) % 10 == 0:
+        #     print("step", epoch, "error:", error)
+
+        if epoch != epoch_size - 1:
+
+            der_b = s.dot(delta)
+            der_w = der_b.dot(x.T)
+
+            weight -= learning_rate * der_w
+            bias -= learning_rate * der_b
+
+    return weight, bias
+
+
+def calc(weight, bias):
+
+    print(sigmoid(weight.dot(x) + bias).tolist())
+    print(y.tolist())
+
+
+weight, bias = train()
+
+print("answer", weight.tolist(), bias.tolist())
+
+print("result:")
+
+calc(weight, bias)
